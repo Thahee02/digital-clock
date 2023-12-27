3414b45fed06646363368d387891a2440c098a6e
HathilThahee <hathilthahee430@gmail.com>
2023-12-27 22:38:26 +0530

    change background

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,20 @@ $("document").ready(function () {
     let hours = today.getHours();
     let minutes = today.getMinutes();
     let seconds = today.getSeconds();
+    let date = today.getDate();
+    let month = (today.getMonth())+1;
+    let year = today.getFullYear();
 
     hours = hours < 10 ? '0' + hours : hours;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
+    date = date < 10 ? '0' + date : date;
+    month = month < 10 ? '0' + month : month;
 
     $("#hours").text(hours);
     $("#minutes").text(minutes);
     $("#seconds").text(seconds);
+    $("#currentDate").text(`${date}-${month}-${year}`);
   }
 
   setInterval(getTime,1000);
